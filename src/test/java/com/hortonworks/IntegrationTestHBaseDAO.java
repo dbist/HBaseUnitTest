c65f28ec438846d9a53c657cb2bd789f8a3c8aa0
Artem Ervits <generic13@gmail.com>
2016-02-09 10:58:40 -0500

    added HBaseIntegrationTest using HBaseTestingUtility

--- src/test/java/com/hortonworks/IntegrationTestHBaseDAO.java
+++ src/test/java/com/hortonworks/IntegrationTestHBaseDAO.java
@@ -15,16 +15,26 @@
  */
 package com.hortonworks;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.hadoop.hbase.HBaseTestingUtility;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
 /**
- *
+ * 
  * @author aervits
+ * 
+ * This code creates an HBase mini-cluster and starts it. Next, it creates a table called MyTest with one column family, CF. A record is inserted, a Get is performed from the same table, and the insertion is verified.
  */
 public class IntegrationTestHBaseDAO {
+    private static final Logger LOG = Logger.getLogger(IntegrationTestHBaseDAO.class.getName());
 
     private static HBaseTestingUtility utility;
     byte[] CF = "CF".getBytes();
@@ -38,19 +48,26 @@ public class IntegrationTestHBaseDAO {
 
     @Test
     public void testInsert() throws Exception {
-        HTableInterface table = utility.createTable(Bytes.toBytes("MyTest"),
+        Table table = utility.createTable(Bytes.toBytes("MyTest"),
                 Bytes.toBytes("CF"));
         HBaseTestObj obj = new HBaseTestObj("testKey", "testValue");
         MyHBaseDAO.insertRecord(table, obj);
         Get get1 = new Get(Bytes.toBytes(obj.getRowKey()));
-        get1.addColumn(CF, CQ1);
+        get1.addColumn(CF, QUALIFIER);
         Result result1 = table.get(get1);
         assertEquals(Bytes.toString(result1.getRow()), obj.getRowKey());
-        assertEquals(Bytes.toString(result1.value()), obj.getData1());
-        Get get2 = new Get(Bytes.toBytes(obj.getRowKey()));
-        get2.addColumn(CF, CQ2);
-        Result result2 = table.get(get2);
-        assertEquals(Bytes.toString(result2.getRow()), obj.getRowKey());
-        assertEquals(Bytes.toString(result2.value()), obj.getData2());
+        assertEquals(Bytes.toString(result1.value()), obj.getValue());
+        
+        LOG.log(Level.INFO, "RowKey: {0}", Bytes.toString(result1.getRow()));
+        LOG.log(Level.INFO, "RowValue: {0}", Bytes.toString(result1.value()));
+    }
+    
+    @After
+    public void tearDown() {
+        try {
+            utility.shutdownMiniCluster();
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, null, ex);
+        }
     }
 }
